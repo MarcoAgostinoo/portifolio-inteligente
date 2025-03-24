180ae6fe05ac4e36cc462f31492a06db0ebaa55e
Sandro Santos <sandro.santos@att.local>
2025-03-23 23:37:50 -0300

    projectandtecimplemented

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,29 +1,29 @@
 // backend/src/app.controller.ts
-import { Controller, Get, Res } from '@nestjs/common';
-import { Response } from 'express';
-import * as path from 'path';
-import * as fs from 'fs';
+import { Controller, Get, Res } from "@nestjs/common"
+import { Response } from "express"
+import * as path from "path"
+import * as fs from "fs"
 
 @Controller()
 export class AppController {
-  @Get()
-  ping(@Res() res: Response): void {
-    // Try different possible paths for index.html
-    const possiblePaths = [
-      path.join(__dirname, '../../public', 'index.html'),
-      path.join(__dirname, '../public', 'index.html'),
-      path.join(__dirname, '../../backend/public', 'index.html'),
-      path.join(__dirname, '../../../public', 'index.html')
-    ];
-    
-    // Find the first path that exists
-    const existingPath = possiblePaths.find(p => fs.existsSync(p));
-    
-    if (existingPath) {
-      res.sendFile(existingPath);
-    } else {
-      // If no file exists, send a simple response
-      res.send(`
+	@Get()
+	ping(@Res() res: Response): void {
+		// Try different possible paths for index.html
+		const possiblePaths = [
+			path.join(__dirname, "../../public", "index.html"),
+			path.join(__dirname, "../public", "index.html"),
+			path.join(__dirname, "../../backend/public", "index.html"),
+			path.join(__dirname, "../../../public", "index.html"),
+		]
+
+		// Find the first path that exists
+		const existingPath = possiblePaths.find((p) => fs.existsSync(p))
+
+		if (existingPath) {
+			res.sendFile(existingPath)
+		} else {
+			// If no file exists, send a simple response
+			res.send(`
         <html>
           <head><title>Portfolio API</title></head>
           <body>
@@ -31,7 +31,7 @@ export class AppController {
             <p>The API is running successfully!</p>
           </body>
         </html>
-      `);
-    }
-  }
+      `)
+		}
+	}
 }
